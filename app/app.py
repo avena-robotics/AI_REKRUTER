42e2bcc76edf13540a4e47105e676eae5325f9a2
Sebastian <sebastian.krajna@pomagier.info>
2024-12-10 08:38:38 +0000

    prod v1.0.0

--- app/app.py
+++ app/app.py
@@ -64,7 +64,6 @@ app = create_app()
 if __name__ == '__main__':
     # Use debug mode from config when running directly
     app.run(
-        host='0.0.0.0', 
         port=5000, 
         debug=Config.DEBUG_MODE
     ) 
